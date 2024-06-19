730650d3f668d8d0920344d2f0e9512bc96e8786
HexPrism <bharathkrishnan33@gmail.com>
2024-06-19 22:30:47 +0530

    Update 2

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-mkdir -p dist/Assets/
-cp -r -f Assets dist/Assets
+mkdir -p dist/assets/
+cp -r -f Assets dist/assets/
 npx github-readme-to-html -i README.md -o index.html -t sbk_portfolio
 cd dist
 # serve .
