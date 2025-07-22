0adb59b4fc7bdaf6af958f860aad2d1504c37e34
Bharath33-Krishnan <bharathkrishnan33@gmail.com>
2025-07-22 13:15:15 +0530

    22-07-2025, finished content for version 1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/Bharath33-Krishnan/sbk_portfolio"
+      href='https://github.com/Bharath33-Krishnan/sbk_portfolio'
       className='link footer__link'
     >
       <p className='footer-copy'>
